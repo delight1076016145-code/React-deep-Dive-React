cbd8769835a0ae7265a203afad3a30fa34d31a57
delight1076016145-code <delight1076016145@gmail.com>
2025-09-08 14:40:19 +0900

    실습 완료

--- ch03/react-generic-1/src/App.tsx
+++ ch03/react-generic-1/src/App.tsx
@@ -1,7 +1,57 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { useEffect, useRef, useState } from "react";
+import { useFetch } from "./hooks/useFetch";
+import { ReactCsspin } from "react-csspin";
 
-export default App
+type TodoItemType = {
+  id: number;
+  todo: string;
+  desc: string;
+  done: boolean;
+};
+
+const App = () => {
+  const [owner, setOwner] = useState<string>("gdhong");
+  const refOwner = useRef<HTMLInputElement | null>(null);
+  const { error, isLoading, response, requestData } = useFetch<TodoItemType[]>(`/todolist_long/${owner}`, { timeout: 5000 });
+
+  const setOwnerHandler = () => {
+    const newOwner = refOwner.current?.value;
+
+    if (newOwner) {
+      setOwner(newOwner);
+    }
+  };
+
+  useEffect(() => {
+    requestData();
+  }, [owner]);
+
+  return (
+    <div>
+      사용자명 : <input type="text" defaultValue={owner} ref={refOwner} />
+      <button onClick={setOwnerHandler}>설정</button>
+      <hr />
+      <h2>{owner}님의 할일 목록</h2>
+      <hr />
+      <ul>
+        {response?.data.map((todoItem: TodoItemType) => {
+          return (
+            <li key={todoItem.id} style={todoItem.done ? { textDecoration: "line-through" } : {}}>
+              {todoItem.todo} - {todoItem.desc} {todoItem.done ? "(완료)" : ""}
+            </li>
+          );
+        })}
+        {isLoading ? <ReactCsspin opacity={0.8} /> : ""},
+        {error ? (
+          <div>
+            <h3>에러 발생 : {error.message}</h3>
+          </div>
+        ) : (
+          ""
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
--- ch03/react-generic-1/src/hooks/useFetch.ts
+++ ch03/react-generic-1/src/hooks/useFetch.ts
@@ -1,7 +1,31 @@
-const useFetch = () => {
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { useState } from "react";
 
-  return {  };
+axios.defaults.baseURL = "/api";
+
+const useFetch = <T>(url: string, params: AxiosRequestConfig) => {
+  const [response, setResponse] = useState<AxiosResponse<T>>();
+  const [error, setError] = useState<AxiosError>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const asyncFatchData = async () => {
+    setResponse(undefined);
+    setIsLoading(true);
+    try {
+      const response: AxiosResponse<T> = await axios.get<T>(url, params);
+      setResponse(response);
+    } catch (error) {
+      setError(error as unknown as AxiosError);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const requestData = () => {
+    asyncFatchData();
+  };
+
+  return { response, error, isLoading, requestData };
 };
-
 
 export { useFetch };
