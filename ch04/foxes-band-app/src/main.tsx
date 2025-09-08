248fcb7287a2c8ec966cd2104c479eb7067d7db2
delight1076016145-code <delight1076016145@gmail.com>
2025-09-08 16:41:10 +0900

    1차 작업 완료 Datarouter API 사용하도록

--- ch04/foxes-band-app/src/main.tsx
+++ ch04/foxes-band-app/src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
 import "./index.css";
 import { BandProvider } from "./BandProvider";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BandProvider>
-      <App />
+      <RouterProvider router={router} />
     </BandProvider>
   </React.StrictMode>
 );
