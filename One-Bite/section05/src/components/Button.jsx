697885e6fc89356ae7ef0c4fc4fe8983b96753ce
delight26 <delight145@naver.com>
2025-12-07 22:57:00 +0900

    이벤트 처리하기

--- One-Bite/section05/src/components/Button.jsx
+++ One-Bite/section05/src/components/Button.jsx
@@ -1,6 +1,16 @@
 const Button = ({children, text, color="black"}) => {
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(`${text} 버튼이 클릭되었습니다.`);
+  }
+
   return (
-    <button style={{ color: color }}>
+    <button 
+      //이벤트 핸들러
+      onClick={onClickButton}
+      onMouseEnter={onClickButton}
+      style={{ color: color }}>
       {text} - {color}
       {children}
     </button>
