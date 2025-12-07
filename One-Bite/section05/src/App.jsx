0d2c8a7dadda5cdcc265f9fc3783adff280c0ca7
delight26 <delight145@naver.com>
2025-12-07 22:47:59 +0900

    Props 강의

--- One-Bite/section05/src/App.jsx
+++ One-Bite/section05/src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main';
 import Footer from './components/footer';
+import Button from './components/Button';
 
 function App() {
 
+  const buttonProps = [
+    { text: "메일", color: "red" },
+    { text: "카페" },
+    { text: "블로그" }
+  ];
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps[0]} />
+      <Button {...buttonProps[1]} />
+      <Button {...buttonProps[2]} >
+        <Header />
+      </Button>
     </>
   )
 }
